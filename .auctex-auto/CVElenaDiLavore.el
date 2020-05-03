ae4e811e5b82b739d41ae41bbfe48af77766eb61
elenadilavore <elenatalita@gmail.com>
2020-05-03 15:13:00 +0300

    update

---
+++ .auctex-auto/CVElenaDiLavore.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "CVElenaDiLavore"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "letterpaper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "svgnames") ("hyphenat" "none")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "xcolor"
+    "framed"
+    "tocloft"
+    "array"
+    "hyphenat")
+   (TeX-add-symbols
+    '("ressubheading" 4)
+    '("resheading" 1)
+    '("resitem" 1))
+   (LaTeX-add-lengths
+    "outerbordwidth")
+   (LaTeX-add-xcolor-definecolors
+    "shadecolor"
+    "shadecolorB"))
+ :latex)
+
