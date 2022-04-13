9a4e1089d28639acc0cb5c0e9f63aa2288eec654
elendi <elendi@ttu.ee>
2022-04-13 17:50:53 +0300

    update cv with publications

--- .auctex-auto/cvOld.el
+++ .auctex-auto/cvOld.el
@@ -1,10 +1,10 @@
 (TeX-add-style-hook
- "CVElenaDiLavore"
+ "cvOld"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "letterpaper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("xcolor" "svgnames") ("hyphenat" "none") ("biblatex" "sorting=ydnt" "maxbibnames=9")))
+                     '(("xcolor" "svgnames") ("hyphenat" "none")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -13,20 +13,12 @@
     "framed"
     "tocloft"
     "array"
-    "hyperref"
-    "hyphenat"
-    "biblatex")
+    "hyphenat")
    (TeX-add-symbols
     '("ressubheading" 4)
     '("resheading" 1)
-    '("resitem" 1)
-    "section")
-   (LaTeX-add-bibliographies
-    "publicationsbib")
+    '("resitem" 1))
    (LaTeX-add-lengths
-    "outerbordwidth")
-   (LaTeX-add-xcolor-definecolors
-    "shadecolor"
-    "shadecolorB"))
+    "outerbordwidth"))
  :latex)
 
